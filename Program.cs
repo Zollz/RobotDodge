a2867bd663a601c3ad4f823e65dc2674f16e7a57
Zollz <eddie_sn@Hotmail.com>
2017-07-13 22:55:57 +1000

    update#3
    program update

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@ public class Program
 {
     public static void Main()
     {
-   
+    Window gameWindow;
+	gameWindow = new Window("Robot Dodge", 700, 700);
+    Bitmap _PlayerBitmap = new Bitmap("Player", "resources/images/player.png");
+    gameWindow.Clear(Color.White);
+    gameWindow.DrawBitmap(_PlayerBitmap, 300, 300);
+    gameWindow.Refresh(60);
+    SplashKit.Delay(10000);
     }
 }
